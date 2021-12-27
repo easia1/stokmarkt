f9b922ee64d6063aed45cd20aed385db78742935
JohnCarreos <jrcarreos1@gmail.com>
2021-12-27 17:57:10 +0800

    Fixed routes and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  
   resources :users
 
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, :path => 'u', :controllers => { registrations: 'users/registrations' }
 
   authenticated do
     root :to => 'users#index', as: :authenticated
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root 'pages#home'
 end
