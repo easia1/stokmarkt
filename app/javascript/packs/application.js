f164301e4570bc6d3383423754e0429c7f922393
Edric Emmanuel Sia <edric.sia@gmail.com>
2021-12-29 14:38:10 +0800

    Added sidebar, account approval, mailer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +14,43 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId);
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show');
+                // change icon
+                toggle.classList.toggle('bx-x');
+                // add padding to body
+                bodypd.classList.toggle('body-pd');
+                // add padding to header
+                headerpd.classList.toggle('body-pd');
+            });
+        }
+    };
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link');
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach((l) => l.classList.remove('active'));
+            this.classList.add('active');
+        }
+    }
+    linkColor.forEach((l) => l.addEventListener('click', colorLink));
+
+    // Your code to run since DOM is loaded and ready
+});
+
+console.log('hello');
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
+  patch 'users/:id/change_status' => 'users#change_status', as: :change_status_user
+
 end
